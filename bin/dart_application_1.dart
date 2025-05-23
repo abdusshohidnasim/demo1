15283e5f521635d0078dab6938129beb5e6ff686
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2025-05-23 03:14:49 -0700

    Initial commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,3 +1,8 @@
-void main(){
-  print("Naiem Hassan Naiem ");
+void main() {
+  Set<int> setA = {1, 2, 3, 4, 5};
+  Set<int> SetB = {4, 5, 6, 7, 8};
+  setA.addAll(SetB);
+  for (var SetC in setA) {
+    print(SetC);
+  }
 }
